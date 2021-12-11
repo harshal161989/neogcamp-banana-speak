d916cd1b38291513a6fd8ea3ebfe2712b86d6c66
harshal161989 <93152305+harshal161989@users.noreply.github.com>
2021-12-11 23:05:03 +0530

    done

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/mandalorian.json"
+
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
+}
 
 function clickEventHandler() {
-  outputDiv.innerText = "ajajajajajaj " + txtInput.value;
+  // outputDiv.innerText = "ajajajajajaj " + txtInput.value;
+ var inputText = txtInput.value;
+ 
+fetch(getTranslationURL(inputText))
+  .then(responce => responce.json())
+  .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+    })
 };
 
 btnTranslate.addEventListener("click",  clickEventHandler) 
